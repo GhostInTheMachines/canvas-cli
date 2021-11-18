0914b19fd143c4b86f9959439dd0e02acb973a96
Dennis Kimble <coder@kimbletechnologies.com>
2021-11-18 21:27:37 +0000

    Fixed Pylint errors

--- get_course_enrollment_student_login_id.py
+++ get_course_enrollment_student_login_id.py
@@ -1,12 +1,10 @@
 """This script is for outputting a list of student login ids from the given course id.
 Course IDs can be obtained by either searching for the course from the Canvas front end,
-or running an SIS export for the department or sub account that you would like a student list from"""
+or running an SIS export for the department or sub account that you would like a student
+list from"""
 # Import modules from InstructureConn video
 import requests
-import json
 
-# Import the Canvas class
-from canvasapi import Canvas
 from vault.dmc_creds import Credentials
 API_URL = Credentials.API_URL
 
@@ -25,3 +23,4 @@ Users = response.json()
 
 for user in Users:
     print(user['user']['login_id'])
+    
